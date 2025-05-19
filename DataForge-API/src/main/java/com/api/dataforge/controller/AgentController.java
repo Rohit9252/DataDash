7e16568e81b17278453ad80469b6bfb779f8df8f
Rohit9252 <rohitbatra0165@gmail.com>
2025-05-19 17:37:15 +0530

    Swagger-ui changes added

--- DataForge-API/src/main/java/com/api/dataforge/controller/AgentController.java
+++ DataForge-API/src/main/java/com/api/dataforge/controller/AgentController.java
@@ -1,7 +1,12 @@
 package com.api.dataforge.controller;
 
+import com.api.dataforge.dto.ErrorResponseDto;
 import com.api.dataforge.service.AgentService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -19,11 +24,38 @@ public class AgentController {
     }
 
     @Operation(summary = "Get all agents", description = "Fetches all agents for the given dataset")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping
     public Mono<?> getAgentsHandler(@RequestParam(required = true) String dataSet){
         return agentService.fetchAgents(dataSet);
     }
-
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @Operation(summary = "Get agent by ID", description = "Fetches a single agent by its ID for the given dataset")
     @GetMapping("/{memberKey}")
     public Mono<?> getAgentByIdHandler(@RequestParam(required = true) String dataSet, @PathVariable String memberKey){
