f25f8b21bd33810fb1dbbe02cdb13e6ef1c2699e
Rohit9252 <rohitbatra0165@gmail.com>
2025-05-19 19:57:26 +0530

    swagger-Controller advice issue dependecy addded

--- DataForge-API/src/main/java/com/api/dataforge/exception/GlobalExceptionHandler.java
+++ DataForge-API/src/main/java/com/api/dataforge/exception/GlobalExceptionHandler.java
@@ -3,26 +3,54 @@ package com.api.dataforge.exception;
 
 import com.api.dataforge.dto.ErrorResponseDto;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+//@ControllerAdvice
+//@RestControllerAdvice
+//@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+
+    @ExceptionHandler(Exception.class)
+    public Mono<ResponseEntity<Map<String, Object>>> handleAllExceptions(
+            Exception ex, ServerWebExchange exchange) {
+        String path = exchange.getRequest().getPath().value();
+        if (path.startsWith("/v3/api-docs") || path.startsWith("/swagger") || path.contains("swagger-ui")) {
+            // Let default handler handle Swagger
+            return Mono.error(ex);
+        }
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Internal Server Error");
+        response.put("message", ex.getMessage());
+        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response));
+    }
+
 
 
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
-                                                                  WebRequest webRequest) {
+                                                                  HttpServletRequest request, WebRequest webRequest) {
+
+
         ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
                 webRequest.getDescription(false),
                 HttpStatus.INTERNAL_SERVER_ERROR,
@@ -43,18 +71,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
     }
-
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(Exception exception,
-//                                                                                 WebRequest webRequest){
-//        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
-//                webRequest.getDescription(false),
-//                HttpStatus.BAD_REQUEST,
-//                exception.getMessage(),
-//                LocalDateTime.now()
-//        );
-//        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
-//    }
 
 
 }
