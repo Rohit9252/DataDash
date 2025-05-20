eda3c8a19c735a7d61353543f4f655801e258382
Chandani <er.chandni99@gmail.com>
2025-05-20 14:17:40 +0530

    swagger issue got fixed

--- DataForge-API/src/main/java/com/api/dataforge/exception/GlobalExceptionHandler.java
+++ DataForge-API/src/main/java/com/api/dataforge/exception/GlobalExceptionHandler.java
@@ -6,19 +6,39 @@ import com.api.dataforge.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
 
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+/*
+    @ExceptionHandler(RuntimeException.class)
+    public Mono<ResponseEntity<ErrorResponseDto>> handleRuntimeException(RuntimeException ex,  ServerWebExchange exchange) {
 
+        String path = exchange.getRequest().getPath().value();
 
+        // ✅ Don't interfere with Swagger/OpenAPI endpoints
+        if (path.startsWith("/v3/api-docs") || path.startsWith("/swagger") || path.contains("swagger-ui")) {
+            return Mono.error(ex);  // Let Spring handle it
+        }
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                path,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(errorResponseDTO));
+    }
+
+ */
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
@@ -43,6 +63,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
     }
+
+
 
 //    @ExceptionHandler(Exception.class)
 //    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(Exception exception,
