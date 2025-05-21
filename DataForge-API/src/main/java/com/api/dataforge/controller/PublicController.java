a4b8037515936b82986dc77cd7423398b55430b0
Rohit9252 <rohitbatra0165@gmail.com>
2025-05-21 13:40:18 +0530

    public and Zillow client swagger documentation completed

--- DataForge-API/src/main/java/com/api/dataforge/controller/PublicController.java
+++ DataForge-API/src/main/java/com/api/dataforge/controller/PublicController.java
@@ -1,7 +1,14 @@
 package com.api.dataforge.controller;
 
 
+import com.api.dataforge.dto.ErrorResponseDto;
 import com.api.dataforge.service.PublicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +19,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/public")
+@Tag(name = "Public", description = "Endpoints for public Data API's")
 public class PublicController {
 
 
@@ -23,23 +31,85 @@ public class PublicController {
     }
 
 
+    @Operation(summary = "Retrieves all parcels", description = "Retrieves all parcels")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/parcels")
     public Mono<?> getPubParcels() {
         return publicService.getPubParcels();
     }
 
+
+    @Operation(summary = "Retrieves parcel by ID", description = "Retrieves parcel by ID")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/parcels/{id}")
     public Mono<?> getParcelById(@PathVariable String id) {
         log.info("Fetching parcel by id: {}", id);
         return publicService.getParcelById(id);
     }
 
+    @Operation(summary = "Retrieves parcel by ID assessments", description = "Retrieves a list of assessments for a given parcel.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/parcels/{id}/assessments")
     public Mono<?> getParcelByIdAssessments(@PathVariable String id) {
         log.info("Fetching parcel by id: {}", id);
         return publicService.getParcelByIdAssessments(id);
     }
 
+
+    @Operation(summary = "Retrieves parcel by ID transactions", description = "Retrieves a list of transactions for a given parcel.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/parcels/{id}/transactions")
     public Mono<?> getParcelByIdTransactions(@PathVariable String id) {
         log.info("Fetching parcel by id: {}", id);
@@ -47,11 +117,42 @@ public class PublicController {
     }
 
 
+    @Operation(summary = "Retrieves a list of assessments.", description = "Retrieves a list of assessments.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/pub/assessments")
     public Mono<?> getPubAssessments() {
         return publicService.getPubAssessments();
     }
 
+
+    @Operation(summary = "Retrieves a list of transactions.", description = "Retrieves a list of transactions.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/pub/transactions")
     public Mono<?> getPubTransactions() {
         return publicService.getPubTransactions();
